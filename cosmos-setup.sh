9d46adc38c3030ed5354bf7343064dc47b45f961
Miguel Nunes <spacemig@gmail.com>
2017-01-04 04:27:13 +0000

    cosmos-setup.sh edited online with Bitbucket

--- cosmos-setup.sh
+++ cosmos-setup.sh
@@ -5,12 +5,17 @@ cosmosBuild='yes'
 dockerBuild='no'
 dockerRun='no'
 downloadRepositories='yes'
-bitbucketUserName='put-your-user-name-here'
+bitbucketUserName='username'
 developer='no' # 'yes' will clone all the repository history (more space used), 'no' will just download the last commit form the master branch (faster download)
 
 echo ""
 echo "------------------"
 echo "COSMOS folder is set to: $cosmosFolder"
+
+# essentials
+# sudo apt-get update
+# sudo apt-get install build-essential cmake git -y # -y tells apt-get not to prompt and just get on with installing
+
 
 if [[ "$downloadRepositories" = "yes" ]]; then
 	./scripts/cosmos-repos.sh $cosmosFolder $bitbucketUserName $developer
